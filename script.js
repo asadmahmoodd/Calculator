1baea8fccb30107db0bae1fa04c3acaeaad8db8e
Asad Mahmood <asadmahmoodd1@gmail.com>
2025-07-31 13:08:36 +0500

    added JS for Inserting Nums

---
+++ script.js
@@ -0,0 +1,33 @@
+const input=document.getElementById("display-num");
+
+
+const nums=document.getElementById("num-btns");
+nums.addEventListener("click",(e)=>
+    {   
+        const number=e.target.id;
+        if(number!='/'&&number!='x'&&number!='+'&&number!='-'&&number!='del'&&number!='num-btns')
+        {   const input=document.getElementById("display-num");
+
+            if(input.value=='0')
+                input.value=number;
+            else
+                input.value=input.value+number;
+        
+        }
+        else if(number=="del"&&input.value!="0")
+        {
+            if(input.value.length!=1)
+            {
+                let str=input.value;
+                str=str.slice(0,-1)
+                input.value=str;
+            }
+            else
+                input.value="0"
+        }
+
+    });
+function insert_num()
+{
+    
+}
