487bfa6366bf8a67c3d30d26407a5bbd01f17af1
Asad Mahmood <asadmahmoodd1@gmail.com>
2025-07-31 16:01:10 +0500

    added calculations

--- script.js
+++ script.js
@@ -5,27 +5,44 @@ const nums=document.getElementById("num-btns");
 nums.addEventListener("click",(e)=>
     {   
         const number=e.target.id;
-        if(number!='/'&&number!='x'&&number!='+'&&number!='-'&&number!='del'&&number!='num-btns'&&number!='equal')
+        if(/*number!='/'&&number!='x'&&number!='+'&&number!='-'&&*/number!='del'&&number!='num-btns'&&number!='equal')
         {   const input=document.getElementById("display-num");
 
-            if(input.value=='0')
-                input.value=number;
-            else
-                input.value=input.value+number;
-        
-        }
-        else if(number=="del"&&input.value!="0")
-        {
-            if(input.value.length!=1)
+            if(input.innerHTML=='0')
             {
-                let str=input.value;
-                str=str.slice(0,-1)
-                input.value=str;
+                if(number!='/'&&number!='x'&&number!='+'&&number!='-')
+                    { input.innerHTML=number;}
             }
             else
-                input.value="0"
+            {
+                input.innerHTML=input.innerHTML+number;
+            }
+        
         }
-
+        else if(number=="del"&&input.innerHTML!="0")
+        {
+            if(input.innerHTML.length!=1)
+            {
+                let str=input.innerHTML;
+                str=str.slice(0,-1)
+                input.innerHTML=str;
+            }
+            else
+                input.innerHTML="0"
+        }
+       else
+       {
+        if(number=='equal')
+        {
+            let expression=input.innerHTML;
+            expression=expression.replaceAll("x","*");
+            let solve=eval(expression);
+            console.log(solve)
+            console.log(solve.toString());
+            input.innerHTML=solve.toString();
+            
+        }
+       }
     });
 function insert_num()
 {
